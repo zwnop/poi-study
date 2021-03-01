8bec43dba19aac4d8e63d53d98f2a7927a46fc8d
zwnop <1043786903@qq.com>
2021-03-02 00:14:09 +0800

    complete

--- src/main/java/org/example/PoiExample.java
+++ src/main/java/org/example/PoiExample.java
@@ -1,15 +1,13 @@
 package org.example;
 
-import org.apache.poi.ss.formula.functions.Column;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.CellCopyPolicy;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.helpers.ColumnHelper;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -19,6 +17,7 @@ import java.io.InputStream;
  */
 public class PoiExample {
     public static final String TEMPLATE_PAH = "template/poi-study.xlsx";
+    public static final String OUTPUT_PAH = "E:\\poi-complete.xlsx";
 
     public static InputStream getInputStream(){
         return Thread.currentThread().getContextClassLoader().getResourceAsStream(TEMPLATE_PAH);
@@ -29,13 +28,35 @@ public class PoiExample {
         XSSFWorkbook book = new XSSFWorkbook(is);
         XSSFSheet sheet = book.getSheetAt(0);
 
-        XSSFRow row = sheet.getRow(0);
+        CellCopyPolicy ccp = new CellCopyPolicy();
+        // 复制总和
+        XSSFRow row1 = sheet.getRow(1);
+        row1.createCell(6);
+        CellRangeAddress region = new CellRangeAddress(1, 2, 6, 7);
+        sheet.addMergedRegion(region);
+        XSSFCell srcCell00 = sheet.getRow(0).getCell(0);
+        XSSFCell cell16 = sheet.getRow(1).getCell(6);
+        cell16.copyCellFrom(srcCell00, ccp);
+        cell16.setCellValue("总和");
 
-        XSSFCell cell = row.getCell(0);
+        // 两列数据举例
+        // copy cells
+        for (int i = 0; i < 40; i++) {
+            XSSFCell srcCell = sheet.getRow(i).getCell(3);
+            XSSFRow row = sheet.getRow(i);
+            XSSFCell cell = row.createCell(4);
+            // 复制单元格
+            cell.copyCellFrom(srcCell, ccp);
+            // TODO 设置值
+            cell.setCellValue(i);
+        }
+        output(book);
     }
 
-    public static void copy(XSSFSheet dest, XSSFCell cell){
-        cell.set
+    public static void output(XSSFWorkbook workbook) throws IOException {
+        FileOutputStream out = new FileOutputStream(OUTPUT_PAH);
+        workbook.write(out);
+        out.close();
     }
 
     public static void main(String[] args) throws IOException {
